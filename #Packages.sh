dfabd864421e75b561f38fe337e457622a3fd720
qhtx <lbxqht@gmail.com>
2025-02-19 23:30:16 +0300

    Init

--- #Packages.sh
+++
@@ -1,4 +0,0 @@
-DEBS_DIR="./debs"
-PACKAGES_FILE="Packages"
-
-apt-ftparchive packages "$DEBS_DIR" > "$PACKAGES_FILE"
--- #Section.sh
+++
@@ -1,8 +0,0 @@
-PACKAGES_FILE="Packages"
-TEMP_FILE="Packages_temp"
-
-if ! grep -q "^Section:" "$PACKAGES_FILE"; then
-    sed -i "1i Section: Tweaks" "$PACKAGES_FILE"
-else
-    sed -i "s/^Section:.*/Section: Tweaks/" "$PACKAGES_FILE"
-fi
