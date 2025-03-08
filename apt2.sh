6e5be662d8a40e93fe80b224897753ff2a59e8af
qhtx <lbxqht@gmail.com>
2025-03-08 22:18:06 +0300

    Init

--- apt2.sh
+++ apt2.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
 BASE_DIR="./debs"
+OUTPUT_FILE="Packages"
 RELEASE_FILE="Release"
 DATE=$(date -Ru)
-ARCHS=("arm" "arm64" "arm64e")
+ARCHS=("iphoneos-arm" "iphoneos-arm64" "iphoneos-arm64e")
 
-> "Packages"
+for deb in "$BASE_DIR"/*.deb; do
+    [ -e "$deb" ] || continue
+
+    name=$(dpkg-deb -f "$deb" Name | tr -d '[:space:]')
+    version=$(dpkg-deb -f "$deb" Version | tr -d '[:space:]')
+    arch=$(dpkg-deb -f "$deb" Architecture | sed 's/^iphoneos-//' | tr -d '[:space:]')
+
+    new_name="${name}.${version}.${arch}.deb"
+    new_path="$BASE_DIR/$arch/$new_name"
+
+    if [ ! -f "$new_path" ]; then
+        mv "$deb" "$new_path"
+    fi
+done
 
 for ARCH in "${ARCHS[@]}"; do
     DEBS_DIR="$BASE_DIR/$ARCH"
-    if [ -d "$DEBS_DIR" ] && [ "$(ls -A "$DEBS_DIR")" ]; then
+    if [ -d "$DEBS_DIR" ]; then
         apt-ftparchive packages "$DEBS_DIR" > "$DEBS_DIR/Packages"
-        cat "$DEBS_DIR/Packages" >> "Packages"
     fi
 done
 
 cat > "$RELEASE_FILE" <<EOF
-Origin:  ‏​‏Λ7M | Repo
-Label:  ‏​‏Λ7M | Repo
+Origin:  ‏​‏  Λ7M | Repo
+Label:  ‏​‏  Λ7M | Repo
 Suite: stable
 Version: 1.0
-Codename:  ‏​‏Λ7M | Repo
+Codename:  ‏​‏  Λ7M | Repo
 Architectures: ${ARCHS[*]}
 Components: main
 Description: rootful & rootless & roothide
@@ -29,15 +42,15 @@ Date: $DATE
 EOF
 
 echo -e "\nMD5Sum:" >> "$RELEASE_FILE"
-find . -maxdepth 1 -type f -name "Packages*" -exec md5sum {} + | awk '{print $1, length($2), $2}' >> "$RELEASE_FILE"
+find "$BASE_DIR" -type f -name "Packages*" -exec md5sum {} + | awk '{print $1, length($2), $2}' >> "$RELEASE_FILE"
 
 echo -e "\nSHA1:" >> "$RELEASE_FILE"
-find . -maxdepth 1 -type f -name "Packages*" -exec sha1sum {} + | awk '{print $1, length($2), $2}' >> "$RELEASE_FILE"
+find "$BASE_DIR" -type f -name "Packages*" -exec sha1sum {} + | awk '{print $1, length($2), $2}' >> "$RELEASE_FILE"
 
 echo -e "\nSHA256:" >> "$RELEASE_FILE"
-find . -maxdepth 1 -type f -name "Packages*" -exec sha256sum {} + | awk '{print $1, length($2), $2}' >> "$RELEASE_FILE"
+find "$BASE_DIR" -type f -name "Packages*" -exec sha256sum {} + | awk '{print $1, length($2), $2}' >> "$RELEASE_FILE"
 
 echo -e "\nSHA512:" >> "$RELEASE_FILE"
-find . -maxdepth 1 -type f -name "Packages*" -exec sha512sum {} + | awk '{print $1, length($2), $2}' >> "$RELEASE_FILE"
+find "$BASE_DIR" -type f -name "Packages*" -exec sha512sum {} + | awk '{print $1, length($2), $2}' >> "$RELEASE_FILE"
 
 echo "✅ Repository updated successfully!"
